247eb59aea416cf1a2e7d5c47c442c44bd3d25c9
RANJIT RAUTARAY <ranjit.r@terralogic.com>
2023-12-03 12:02:27 +0530

    home page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import Homepage from "./components/Home"
 
 export default function Home() {
   return (
     <main >
      <h1>this is next js app</h1>
      <p>we are using this for practice</p>
+     <Homepage />
     </main>
   )
 }
